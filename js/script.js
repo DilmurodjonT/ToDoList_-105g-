d0afb71201008868eee5c84dd80c215c57beb6fc
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-03-03 22:06:35 +0500

    deficiencies have been filled

--- js/script.js
+++ js/script.js
@@ -7,9 +7,24 @@ $form.addEventListener("submit", (e) => {
   //   console.log($todoInput.value);
   const $taskMainElement = document.createElement("div");
   const $taskMainTitle = document.createElement("p");
+  const $btnsWrapperElement = document.createElement("div");
   $taskMainTitle.innerText = $todoInput.value;
   $taskMainElement.appendChild($taskMainTitle);
   $taskMainElement.className = "task-item";
   $taskContainer.appendChild($taskMainElement);
+  $btnsWrapperElement.className = "task-item__btn-wrapper";
+  $btnsWrapperElement.innerHTML = `
+  <button class="complete"> <i class="fas fa-circle-check"></i> <br> Complete </button>
+  <button> <i class="fas fa-edit"></i> <br> Edit </button>
+  <button> <i class="fas fa-clock"></i> <br> 9:41:10 </button>
+  <button> <i class="fas fa-trash"></i> <br> Delete </button>
+  `;
+  $taskMainElement.appendChild($btnsWrapperElement);
   $todoInput.value = "";
+});
+
+$taskContainer.addEventListener("click", (e) => {
+  if (e.target.className.includes("complete")) {
+    e.target.parentElement.previousSibling.classList.toggle("completed");
+  }
 });
