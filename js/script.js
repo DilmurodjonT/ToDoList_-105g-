4f3b9f33bd8aee8d53d55600a3b1c676822d950a
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-03-04 19:35:34 +0500

    scss and js completed

--- js/script.js
+++ js/script.js
@@ -4,27 +4,66 @@ const $taskContainer = document.querySelector("#task-container");
 
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
-  //   console.log($todoInput.value);
-  const $taskMainElement = document.createElement("div");
-  const $taskMainTitle = document.createElement("p");
-  const $btnsWrapperElement = document.createElement("div");
-  $taskMainTitle.innerText = $todoInput.value;
-  $taskMainElement.appendChild($taskMainTitle);
-  $taskMainElement.className = "task-item";
-  $taskContainer.appendChild($taskMainElement);
-  $btnsWrapperElement.className = "task-item__btn-wrapper";
-  $btnsWrapperElement.innerHTML = `
+  if ($todoInput.value.trim().length > 1) {
+    //   console.log($todoInput.value);
+    const time = new Date();
+    const $taskMainElement = document.createElement("div");
+    const $taskMainTitle = document.createElement("p");
+    const $btnsWrapperElement = document.createElement("div");
+    $taskMainTitle.innerText = $todoInput.value;
+    $taskMainElement.appendChild($taskMainTitle);
+    $taskMainElement.className = "task-item";
+    $taskContainer.appendChild($taskMainElement);
+    $btnsWrapperElement.className = "task-item__btn-wrapper";
+    $btnsWrapperElement.innerHTML = `
   <button class="complete"> <i class="fas fa-circle-check"></i> <br> Complete </button>
-  <button> <i class="fas fa-edit"></i> <br> Edit </button>
-  <button> <i class="fas fa-clock"></i> <br> 9:41:10 </button>
-  <button> <i class="fas fa-trash"></i> <br> Delete </button>
+  <button class="edit"> <i class="fas fa-edit"></i> <br> Edit </button>
+  <button> <i class="fas fa-clock"></i> <br> 
+  ${time.getHours().toString().padStart(2, "0")}:${time
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}:${time
+      .getSeconds()
+      .toString()
+      .padStart(2, "0")} </button>
+  <button class="delete"> <i class="fas fa-trash"></i> <br> Delete </button>
   `;
-  $taskMainElement.appendChild($btnsWrapperElement);
-  $todoInput.value = "";
+    $taskMainElement.appendChild($btnsWrapperElement);
+    $todoInput.value = "";
+  }
 });
 
 $taskContainer.addEventListener("click", (e) => {
-  if (e.target.className.includes("complete")) {
+  if (e.target.className == "complete") {
     e.target.parentElement.previousSibling.classList.toggle("completed");
+  } else if (e.target.className == "delete") {
+    // console.log("bosildi");
+    const isAgreedToDelete = confirm(
+      "Siz bu postni o'chirib tashlashga rozimisiz?"
+    );
+    if (isAgreedToDelete) {
+      e.target.parentElement.parentElement.classList.add("remove-item");
+      setTimeout(() => {
+        e.target.parentElement.parentElement.remove();
+      }, 300);
+    }
+  } else if (e.target.className == "edit") {
+    // console.log("bosildi");
+    if (
+      e.target.parentElement.previousSibling.hasAttribute("contenteditable")
+    ) {
+      e.target.parentElement.previousSibling.removeAttribute("contenteditable");
+      e.target.parentElement.previousSibling.classList.remove("changing");
+      e.target.innerHTML = '<i class="fas fa-edit"></i> <br> Edit';
+      e.target.style.background = "gold";
+    } else {
+      e.target.parentElement.previousSibling.setAttribute(
+        "contenteditable",
+        true
+      );
+      e.target.parentElement.previousSibling.classList.add("changing");
+      e.target.innerHTML = '<i class="fas fa-check-double"></i> <br> Done';
+      e.target.style.background = "purple";
+    }
   }
 });
